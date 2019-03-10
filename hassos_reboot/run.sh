649511e888bcdc63686ab6aa4a910a0b22eb6e7e
coolbombom <cool_bombom@hotmail.com>
2019-03-10 15:48:33 +0100

    Create run.sh

---
+++ hassos_reboot/run.sh
@@ -0,0 +1,14 @@
+hassio host reboot
+
+#!/bin/sh
+
+#FILE="/share/hassos.rb"
+
+#while true; do
+#    if [ -f $FILE ]; then
+#        rm /share/hassos.rb
+#        hassio host reboot
+#    fi
+
+#    sleep 5
+#done
